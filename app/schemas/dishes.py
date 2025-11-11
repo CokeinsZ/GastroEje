28695c486f6bc3b3797945998777bc71fa8c4ecd
LFSalazar <luifersalazar.18@gmail.com>
2025-11-11 17:51:45 -0500

    CRUD_Dishes
    CRUD

--- app/schemas/dishes.py
+++ app/schemas/dishes.py
@@ -11,6 +11,13 @@ class DishBase(BaseModel):
 class DishCreate(DishBase):
     pass
 
+class DishUpdate(BaseModel):
+    menu_id: Optional[int] = None
+    name: Optional[str] = None
+    description: Optional[str] = None
+    price: Optional[float] = None
+    img: Optional[str] = None
+
 class DishOut(DishBase):
     dish_id: int
     model_config = ConfigDict(from_attributes=True)
